39afcc6264291ab4ac6d5dd0eb47b326d0e56269
PalakGoyal123 <64060763+PalakGoyal123@users.noreply.github.com>
2022-11-30 11:07:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './Component/Sidebar/Sidebar';
+import Home from './Component/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar/>
+      <Home/>
+       </div>
+  );
+}
+
+export default App;
